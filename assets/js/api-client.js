e65fbe3e246bf4aab42413bee104f473a392e7ce
Jessica Michel <jessicamichel93@gmail.com>
2020-11-17 11:39:29 +0100

    add successful POST request

--- assets/js/api-client.js
+++ assets/js/api-client.js
@@ -13,3 +13,24 @@ const getData = (endpoint) => {
     }
 }
 getData();
+
+// POST
+const data = { description: "water plants", done: false };
+const addTodo = () => {
+    try {
+        fetch(API_URL, {
+                method: "POST",
+                body: JSON.stringify(data),
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+            })
+    } catch (error) {
+        console.log(error);
+    }
+}
+addTodo();
