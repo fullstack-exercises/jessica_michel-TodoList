53df151efac23674d9846ab071a9382f3510bf5c
Jessica Michel <jessicamichel93@gmail.com>
2020-11-21 14:00:55 +0100

    adding label inside form toDo List

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,24 @@
 import { getData, postTodo, deleteTodo } from './api-client.js';
 
 const container = document.querySelector('.container');
-const toDoList = document.querySelector('.todo-list');
-const formTodo = document.querySelector('.form-todo');
+const toDoList = document.querySelector('.todo__list');
+const formTodo = document.querySelector('.todo__form');
 const inputTodo = document.querySelector('.input-todo');
-const idTodo = document.getElementsByClassName('id');
 
 // Get data from another file
 const addTodo = async() => {
     const data = await getData();
     const getTodo = data.map(item => {
-        const todo = item.description;
-        let li = document.createElement('li');
-        li.setAttribute('id', item._id); // creates ID
-        toDoList.appendChild(li);
-        li.classList.add('task');
-        li.innerHTML = item.description;
+
+        const newform = document.createElement('form');
+        newform.setAttribute('id', item._id); // creates ID
+
+        const label = document.createElement('label');
+        label.innerHTML = item.description;
+        label.classList.add('todo__single');
+
+        toDoList.appendChild(newform);
+        newform.appendChild(label);
     });
 }
 addTodo();
